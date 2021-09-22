8b14d18336c979be2e61d8937116ef5b71da5ed8
unknown <lehoquocbao9@gmail.com>
2021-09-22 16:16:54 +0700

    add project

---
+++ myDoctor/Controllers/DoctorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myDoctor.Controllers
+{
+    public class DoctorController : Controller
+    {
+        // GET: Doctor
+        public ActionResult Doctor()
+        {
+            return View();
+        }
+    }
+}
